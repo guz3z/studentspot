8532c6522d3c02d998217e780faa96afb46b7257
Zerh Guzman <zerhguzman@gmail.com>
2021-12-30 16:51:25 +0000

    setup structure of auth client

--- client/src/components/Auth/Auth.jsx
+++ client/src/components/Auth/Auth.jsx
@@ -4,8 +4,78 @@ import axios from 'axios'
 import './auth.css'
 
 export function Auth() {
+
+    const [ isSignUp, setIsSignUp ] = useState(true);
+
+    function switchMode(){
+        setIsSignUp((prevValue) => !prevValue )
+    }
+
+    function handleChange(){}
+
+
+
     return (
-        <div>
+        <div className='auth-form-cont'>
+            <div className="auth-form-cont-fields">
+                <div className="auth-form-cont-fields-content">
+                    <p>{ isSignUp ? 'Register' : 'Log in'}</p>
+                    <form action="" onSubmit={() => {}}>
+                        { isSignUp && (
+                            <div className="auth-form-cont-fields-cont-input">
+                                <label htmlFor="fullName">Full Name</label>
+                                <input 
+                                    type="text"
+                                    name='fullName'
+                                    placeholder='Full name'
+                                    onChange={handleChange}
+                                    required
+                                />
+                            </div>
+                        )}
+                        <div className="auth-form-cont-fields-cont-input">
+                            <label htmlFor="email">Email</label>
+                            <input 
+                                type="text"
+                                name='email'
+                                placeholder='Email'
+                                onChange={handleChange}
+                                required
+                            />
+                        </div>
+                        <div className="auth-form-cont-fields-cont-input">
+                            <label htmlFor="password">Password</label>
+                            <input 
+                                type="password"
+                                name='password'
+                                placeholder='Password'
+                                onChange={handleChange}
+                                required
+                            />
+                        </div>
+                        { isSignUp && (
+                            <div className="auth-form-cont-fields-cont-input">
+                                <label htmlFor="confirmPassword">Confirm Password</label>
+                                <input 
+                                    type="password"
+                                    name='confirmPassword'
+                                    placeholder='Confirm Password'
+                                    onChange={handleChange}
+                                    required
+                                />
+                            </div>
+                        )}
+                    </form>
+                    <div className="auth-form-cont-account">
+                        <p>
+                            { isSignUp ? 'Already have an account?' : 'Join us!' }
+                            <span onClick={switchMode}>
+                                { isSignUp ? 'Log in' : 'Register' }
+                            </span>
+                        </p>
+                    </div>
+                </div>
+            </div>
             
         </div>
     )
--- client/src/pages/HomePage/HomePage.jsx
+++ client/src/pages/HomePage/HomePage.jsx
@@ -1,40 +1,50 @@
 import React from 'react';
+import { Auth } from '../../components/index'
 import './homepage.css'
 
 
 export function HomePage() {
     return (
-       
+       <>
 
-        <div className="container-fluid">
-            <div className='hp-header'>
-                <img className='hp-logo' src={require('../../assets/img/logowhiteSSpot.png')}/>
-                <nav>
-                    <ul>
-                        <li><a href="#" id="Home">Home</a></li>
-                        <li><a href="#" id="About">About</a></li>
-                        <li><a href="#" id="Join">Join</a></li>
-                        <li><a href="#" id="Contact">Contact</a></li>
-                    </ul>
-                </nav>
-            </div>
+            <div className="container-fluid" id='sectionHomeP'>
+                <div className='hp-header'>
+                    <img className='hp-logo' src={require('../../assets/img/logowhiteSSpot.png')}/>
+                    <nav>
+                        <ul>
+                            <li><a href="#sectionHomeP" id="Home">Home</a></li>
+                            <li><a href="#sectionAbout" id="About">About</a></li>
+                            <li ><a href="#sectionJoin" id="Join" >Log in</a></li>
+                            <li><a href="#sectionContact" id="Contact">Contact</a></li>
+                        </ul>
+                    </nav>
+                </div>
 
-            
-            <div className="homepage-bg">
-            <div className='intro-cont'>
-                <h1>Welcome to</h1>
-                <h2>StudentSpot</h2>
-                <p>A DYNAMIC STUDENT ENGAGEMENT EXPERIENCE</p>
-            </div>
                 
-                <div className="hp-cube"></div>
-                <div className="hp-cube"></div>
-                <div className="hp-cube"></div>
-                <div className="hp-cube"></div>
-                <div className="hp-cube"></div>
+                <div className="homepage-bg">
+                <div className='intro-cont'>
+                    <h1>Welcome to</h1>
+                    <h2>StudentSpot</h2>
+                    <p>A DYNAMIC STUDENT ENGAGEMENT EXPERIENCE</p>
+                </div>
+                    
+                    <div className="hp-cube"></div>
+                    <div className="hp-cube"></div>
+                    <div className="hp-cube"></div>
+                    <div className="hp-cube"></div>
+                    <div className="hp-cube"></div>
+                </div>
+                
             </div>
-            
-        </div>
+
+            <div id="sectionJoin" className='hp-join-cont'>
+                <Auth />
+
+            </div>
+
+        </>
+
+
         
     )
 };
