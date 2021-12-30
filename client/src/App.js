a6f15a4ae30d57a6a8338a486b3b8c96cba704ab
Zerh Guzman <zerhguzman@gmail.com>
2021-12-30 12:56:11 +0000

    add the routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import { StreamChat } from 'stream-chat';
 import { Chat } from 'stream-chat-react';
 import Cookies from 'universal-cookie';
-import { HomePage } from './pages';
+import { HomePage, ChatRoom } from './pages';
 import './style.css'
 
 const apiKey = '68z8xjsd4atu';
@@ -12,10 +13,16 @@ const client = StreamChat.getInstance(apiKey);
 function App() {
     return (
         <div className='app-cont'>
-            <HomePage />
-            <Chat client={client} theme='team light'>
 
-            </Chat>
+            <Switch>
+                <Route exact path='/'><HomePage /></Route>
+                <Route path='/chatroom'>
+                    <Chat client={client} theme='team light'>
+                        <ChatRoom />
+                    </Chat>
+                </Route>
+                
+            </Switch>
             
         </div>
     )
