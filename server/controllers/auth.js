f3e7c6e197a7cdf1dc4f396fd8b77ab58eecffa1
Zerh Guzman <zerhguzman@gmail.com>
2021-12-30 19:50:51 +0000

    add the auth for backend

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -6,10 +6,10 @@ const crypto = require('crypto');
 
 const api_key = process.env.STREAM_API_KEY;
 const api_secret = process.env.STREAM_API_SECRET;
-const api_id = process.env.STREAM_API_ID;
+const app_id = process.env.STREAM_API_ID;
 
 
-const signup = (req, res) => {
+const signup = async (req, res) => {
     try{
         const { fullName, email, password } = req.body
 
@@ -17,13 +17,41 @@ const signup = (req, res) => {
 
         const serverClient = connect(api_key, api_secret, app_id);
 
+        const hashedPassword = await bcrypt.hash(password, 10); //a new user will have a hashedPassword created for them
+
+        const token = serverClient.createUserToken(userId);
+
+        res.status(200).json({ token, fullName, email, userId, hashedPassword });
+
     } catch(err) {
         res.status(500).json({ message: err });
     }
 }
 
-const login = (req, res) => {
+const login = async (req, res) => {
     try{
+        const { email, password } = req.body;
+
+        const serverClient = connect(api_key, api_secret, app_id);
+
+        const client = StreamChat.getInstance(api_key, api_secret);
+
+        const { users } = await client.queryUsers({ email: email }); //taking the email and query all the users in the database to see if an email matches
+
+        if(!users.length) return res.status(400).json({ message: 'User not found' });
+
+        const success = await bcrypt.compare(password, users[0].hashedPassword);
+
+        const token = serverClient.createUserToken(users[0].id);
+
+        if(success) {
+            res.status(200).json({ token, fullName: users[0].fullName, email, userId: users[0].id});
+        } else {
+            res.status(500).json({ message: 'Incorrect password' })
+
+        }
+
+
 
     } catch(err) {
         res.status(500).json({ message: err });
