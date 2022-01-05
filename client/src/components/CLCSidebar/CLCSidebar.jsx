a2e96d878827b82faea2f593f3a2e0b68f7b1f68
Zerh Guzman <zerhguzman@gmail.com>
2022-01-05 16:23:15 +0000

    log out works

--- client/src/components/CLCSidebar/CLCSidebar.jsx
+++ client/src/components/CLCSidebar/CLCSidebar.jsx
@@ -1,15 +1,29 @@
 import React from 'react';
-import './clcsidebar.css'
+import Cookies from 'universal-cookie';
+import './clcsidebar.css';
+
+const cookies = new Cookies();
 
 export function CLCSidebar() {
+
+    const logout = () => {
+        cookies.remove('token');
+        cookies.remove('userId');
+        cookies.remove('fullName');
+        cookies.remove('email');
+        cookies.remove('hashedPassword');
+
+        window.location.href = 'http://localhost:3000'
+    }
+
     return (
         <div className='clcsb-cont'>
             <div className="clcsb-icon-cont">
                 <div className="clcsb-icon">
-                    <a href='http://localhost:3000/mainfeed'><i class="fas fa-th-large"></i></a>
+                    <a href='http://localhost:3000/mainfeed'><i className="fas fa-th-large"></i></a>
                 </div>
                 <div className="clcsb-icon">
-                    <i className="fas fa-sign-out-alt"></i>
+                    <i className="fas fa-sign-out-alt" onClick={logout}></i>
                 </div>
             </div>
             
